0bce8f99379b9d823d53d17eaee7b1a782f32098
rizkyalviann <rzkyalvn1954@gmail.com>
2023-03-20 20:17:34 +0700

    tugas 2

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Rizky Alvian
+        Saya tinggal di kreo olahraga berenang";
+
+    }
+}
