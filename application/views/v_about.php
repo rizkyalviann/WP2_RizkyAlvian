43230b1805524115bb2efa4bd49f98a14460a3d0
rizkyalviann <rzkyalvn1954@gmail.com>
2023-03-27 18:23:34 +0700

    tugas 3

---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Rizky</li>
+        <li>Nama Belakang : Alvian</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jalan Kreo Ciledug No.89 Tangerang</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Surabaya</li>
+    </ul>
+
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Futsal</li>
+        <li>Bulutangkis</li>
+    </ul>
+</section>
